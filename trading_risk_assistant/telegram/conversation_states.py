23ad9cbcec8a911e830ac6580143be7a4aefded6
braram99 <br792346@gmail.com>
2025-10-05 13:08:07 -0400

    Update conversation_states.py

---
+++ trading_risk_assistant/telegram/conversation_states.py
@@ -0,0 +1,14 @@
+# Estados para la conversación /risk
+
+SYMBOL = 0
+SESSION = 1
+BALANCE = 2
+SL_PIPS = 3
+PIP_VALUE = 4
+QUALITY = 5
+CONFLUENCIAS = 6
+PSYCHOLOGY = 7
+RISK_RESULT = 8
+
+# Puedes importar con:
+# from telegram.conversation_states import SYMBOL, SESSION, etc.
